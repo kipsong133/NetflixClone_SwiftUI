3a94a01152abf37d37f2dd60eeea0c9f607050f4
KimWooSun <kipsong133@gmail.com>
2021-12-07 10:06:10 +0900

    Initial Commit

---
+++ NetflixClone_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetflixClone_SwiftUI
+//
+//  Created by 김우성 on 2021/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetflixClone_SwiftUI/NetflixClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixClone_SwiftUIApp.swift
+//  NetflixClone_SwiftUI
+//
+//  Created by 김우성 on 2021/12/07.
+//
+
+import SwiftUI
+
+@main
+struct NetflixClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
