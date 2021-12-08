dfa877b6888a2e2e9468364785fd2f7b0cf53480
KimWooSun <kipsong133@gmail.com>
2021-12-08 12:25:45 +0900

    feat: add Top Banner View

---
+++ NetflixClone_SwiftUI/Home/TopMoviePreview.swift
@@ -0,0 +1,62 @@
+//
+//  TopMoviePreview.swift
+//  NetflixClone_SwiftUI
+//
+//  Created by 김우성 on 2021/12/08.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct TopMoviePreview: View {
+    var movie: Movie
+    
+    func isCategoryLast(_ cat: String) -> Bool {
+        let catCount = movie.categories.count
+        
+        if let index = movie.categories.firstIndex(of: cat) {
+            if index + 1  != catCount {
+                return false
+            }
+        }
+        return true
+    }
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(movie.categories, id: \.self) { category in
+                        
+                        HStack {
+                            Text(category)
+                            
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 3))
+                            }
+                            
+                        }
+                    }
+                }
+                
+                Text("Row of buttons")
+            }
+        }.foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
+ 
--- NetflixClone_SwiftUI/StandardHomeMovie.swift
+++ NetflixClone_SwiftUI/StandardHomeMovie.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import Kingfisher
 
 struct StandardHomeMovie: View {
-    
     var movie: Movie
     
     var body: some View {
